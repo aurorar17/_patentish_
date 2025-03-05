11367bfe4463fd4cd7257843691b0a53537ff9fa
aurorar17 <aurora.roilo01@gmail.com>
2025-03-05 00:14:13 -0800

    completion .gitignore

---
+++ BACKEND/manage.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import os
+import sys
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+    # ... resto del file invariato ... 
